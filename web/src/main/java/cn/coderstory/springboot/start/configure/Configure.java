d5ab2b4e38e71e50d5b07910db55a613fb5d28d5
coderstory <coderstory@163.com>
2019-11-11 17:44:36 +0800

    RestTemplate & Ribbon

--- web/src/main/java/cn/coderstory/springboot/start/configure/Configure.java
+++ web/src/main/java/cn/coderstory/springboot/start/configure/Configure.java
@@ -1,7 +1,15 @@
 package cn.coderstory.springboot.start.configure;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class Configure {
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
--- web/src/main/java/cn/coderstory/springboot/start/controller/HomeController.java
+++ web/src/main/java/cn/coderstory/springboot/start/controller/HomeController.java
@@ -3,16 +3,25 @@ package cn.coderstory.springboot.start.controller;
 import cn.coderstory.springboot.start.configure.TestConfigBean;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/home")
 @EnableConfigurationProperties()
 public class HomeController {
     TestConfigBean bean;
+    RestTemplate template;
+    LoadBalancerClient loadBalancerClient;
 
-    public HomeController(TestConfigBean bean) {
+    public HomeController(TestConfigBean bean, RestTemplate template, LoadBalancerClient loadBalancerClient) {
         this.bean = bean;
+        this.template = template;
+        this.loadBalancerClient = loadBalancerClient;
     }
 
     @ApiOperation("hello")
@@ -20,4 +29,14 @@ public class HomeController {
     String index() {
         return bean.getName();
     }
+
+    /**
+     * 通过ribbon实现负载均衡 RestTemplate通过服务名调用服务
+     */
+    @GetMapping("/access")
+    String accessService() {
+        ServiceInstance instance = loadBalancerClient.choose("USER-SERVICE");
+        return instance.getHost() + instance.getPort() + template.getForObject("http://USER-SERVICE/", String.class);
+    }
+
 }
